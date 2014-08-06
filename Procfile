67fcbaf3bea045cac9ae04e94c8034627cf1b581
Matt Graham <kickthebox@gmail.com>
2014-08-06 16:33:50 -0400

    base

---
+++ Procfile
@@ -0,0 +1,2 @@
+php:  php -S 0.0.0.0:8000
+compass: compass watch
---
+++ config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/assets/css"
+sass_dir = "/assets/scss"
+images_dir = "/assets/images"
+javascripts_dir = "/assets/js"
+
+output_style = :expanded
+line_comments = false
